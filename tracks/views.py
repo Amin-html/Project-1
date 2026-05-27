5db77d6708a3054d04f9d1aca8b14e33bf7c9754
Amin911 <adilov.amin.rx7.a86@gmail.com>
2026-05-28 05:16:53 +0600

    feat: добавил страницы треков

--- tracks/views.py
+++ tracks/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Track
 
+def track_list(request):
+    tracks = Track.objects.all()
+    return render(request, 'tracks/track_list.html', {'tracks': tracks})
+
+def track_detail(request, pk):
+    track = get_object_or_404(Track, pk=pk)
+    return render(request, 'tracks/track_detail.html', {'track': track})
 # Create your views here.
