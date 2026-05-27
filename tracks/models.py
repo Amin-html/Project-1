9d4a02bbac49cedbad35df98f17f9ddca15618ac
Amin911 <adilov.amin.rx7.a86@gmail.com>
2026-05-28 04:26:16 +0600

    feat: добавил модели Track и Booking

--- tracks/models.py
+++ tracks/models.py
@@ -2,4 +2,11 @@ from django.db import models
 
 class Track(models.Model):
     name = models.CharField(max_length=100)
+    description = models.TextField()
+    price_per_hour = models.IntegerField(default=0)
+    length_km = models.FloatField(default=0)
+    image = models.ImageField(upload_to='tracks/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 # Create your models here.
