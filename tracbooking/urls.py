74c3e48ce8995eb42ccca19867408d6b007d6f0a
Amin911 <adilov.amin.rx7.a86@gmail.com>
2026-05-29 17:07:35 +0600

    finish: проект завершён, добавил README

--- tracbooking/urls.py
+++ tracbooking/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: redirect('track_list')),              # главная → треки
     path('tracks/', include('tracks.urls')),
     path('bookings/', include('bookings.urls')),
     path('accounts/', include('accounts.urls')),
